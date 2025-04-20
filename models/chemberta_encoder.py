6b621ff4fa60fd0fe8ec106111085e5894be94a8
bmwoolf <bradleymwoolf@gmail.com>
2025-04-20 16:18:58 -0700

    chemberta class- pretrained transformer on SMILES

---
+++ models/chemberta_encoder.py
@@ -0,0 +1,14 @@
+from transformers import AutoModel, AutoTokenizer
+
+class ChemBERTaEncoder(nn.Module):
+    def __init__(self, model_name='seyonec/ChemBERTa-zinc-base-v1'):
+        super().__init__()
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModel.from_pretrained(model_name)
+        self.linear = nn.Linear(self.model.config.hidden_size, 128)
+
+    def forward(self, smiles_list):
+        encoded = self.tokenizer(smiles_list, return_tensors='pt', padding=True, truncation=True)
+        output = self.model(**encoded)
+        pooled = output.last_hidden_state[:, 0]  # CLS token
+        return self.linear(pooled)
